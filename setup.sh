85ed4fa72bcd3543e542d035dfeb3cfc4496dbaa
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-10-17 12:07:04 +0200

    updated to check evironmet variable and adding force option

--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
 # setting up neccessary libraries
-LHA_BASE=`scram tool info lhapdf | grep LHAPDF_BASE | sed 's/.*=\(.*\)/\1/'`
-source $LHA_BASE/etc/profile.d/init.sh
+command -v scram > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "scarm doesn't exist, run cmsenv"
+  exit 1
+fi
+if [ -z "$LHAPDF_ROOT" -o $1 == "-f" ]; then
+  echo "sourcing lhapdf from scram tool info lhapdf"
+  LHA_BASE=`scram tool info lhapdf | grep LHAPDF_BASE | sed 's/.*=\(.*\)/\1/'`
+  source $LHA_BASE/etc/profile.d/init.sh
+else
+  echo "already existing, doing nothing \n resetting it by calling with force -f "
+fi
