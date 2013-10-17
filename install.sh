836c2aaef15822f6caf42843162f3854a8c1eba9
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-10-17 18:17:08 +0200

    included patch in install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
-installDir=$PWD/MCatNLO_4_1_0_Base
-tarBallDir=$PWD/mcatnlo_410_tarBall
+mainDir=$PWD
+installDir=$mainDir/MCatNLO_4_1_0_Base
+tarBallDir=$mainDir/mcatnlo_410_tarBall
 mkdir $tarBallDir
 cd $tarBallDir
 wget http://www.hep.phy.cam.ac.uk/theory/webber/MCatNLO/Package4.10_dist.tar.gz
 mkdir $installDir
 cd $installDir
 tar -xzf $tarBallDir/Package4.10_dist.tar.gz
-
+patch < $mainDir/cmsswMCatNLO_4_1_0.patch
